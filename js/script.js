8dacaf8e77b95a6fc97ac5cfcaf2a70ff846cd2c
taiyeba-software <islamtaiyeba38@gmail.com>
2024-11-12 14:51:49 +0600

    banner added have a little problem

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//header scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll=function()
+{
+    if(document.documentElement.scrollTop>20) //The documentElement property returns a document's element (as an Element object).//
+    {
+
+    }
+}
